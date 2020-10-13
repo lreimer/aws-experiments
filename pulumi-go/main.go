078ea56511ba97a5ad24c5ffb4b42e5396edaffb
Mario-Leander Reimer <mario-leander.reimer@qaware.de>
2020-10-13 10:43:54 +0200

    Added index HTML to S3 bucket.

--- pulumi-go/main.go
+++ pulumi-go/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"io/ioutil"
+
 	"github.com/pulumi/pulumi-aws/sdk/v3/go/aws/s3"
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
@@ -9,6 +11,19 @@ func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		// Create an AWS resource (S3 Bucket)
 		bucket, err := s3.NewBucket(ctx, "pulumi-bucket", nil)
+		if err != nil {
+			return err
+		}
+
+		htmlContent, err := ioutil.ReadFile("site/index.html")
+		if err != nil {
+			return err
+		}
+
+		_, err = s3.NewBucketObject(ctx, "index.html", &s3.BucketObjectArgs{
+			Bucket:  bucket.ID(),
+			Content: pulumi.String(string(htmlContent)),
+		})
 		if err != nil {
 			return err
 		}
