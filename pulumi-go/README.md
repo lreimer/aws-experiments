e3b0274d676b5ebee5cfbd04f7eea420db66d11a
Mario-Leander Reimer <mario-leander.reimer@qaware.de>
2020-10-13 10:53:57 +0200

    Publish HTML page with S3.

---
+++ pulumi-go/README.md
@@ -0,0 +1,3 @@
+# Infrastructure as Code using Pulumi and Go
+
+This demo is simply the code from the Pulumi getting started with Go.
--- pulumi-go/main.go
+++ pulumi-go/main.go
@@ -10,7 +10,11 @@ import (
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		// Create an AWS resource (S3 Bucket)
-		bucket, err := s3.NewBucket(ctx, "pulumi-bucket", nil)
+		bucket, err := s3.NewBucket(ctx, "pulumi-bucket", &s3.BucketArgs{
+			Website: s3.BucketWebsiteArgs{
+				IndexDocument: pulumi.String("index.html"),
+			},
+		})
 		if err != nil {
 			return err
 		}
@@ -21,15 +25,19 @@ func main() {
 		}
 
 		_, err = s3.NewBucketObject(ctx, "index.html", &s3.BucketObjectArgs{
-			Bucket:  bucket.ID(),
-			Content: pulumi.String(string(htmlContent)),
+			Acl:         pulumi.String("public-read"),
+			ContentType: pulumi.String("text/html"),
+			Bucket:      bucket.ID(),
+			Content:     pulumi.String(string(htmlContent)),
 		})
 		if err != nil {
 			return err
 		}
 
-		// Export the name of the bucket
+		// Export the name and URL of the bucket
 		ctx.Export("bucketName", bucket.ID())
+		ctx.Export("bucketEndpoint", pulumi.Sprintf("http://%s", bucket.WebsiteEndpoint))
+
 		return nil
 	})
 }
